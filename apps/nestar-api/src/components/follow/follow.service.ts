87a8c7c99a59e042dff77803f4396f5a6157d875
adam01051 <lloki0105@gmail.com>
2026-04-24 17:36:06 +0900

    feat: create followe resolver and service

---
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Follower, Following } from '../../libs/dto/follow/follow';
+
+@Injectable()
+export class FollowService {
+	@InjectModel('Follow') private readonly followModel: Model<Follower | Following>;
+	private readonly memberService: MemberService;
+}
