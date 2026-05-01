91025bf148ec3d02714d5bed9ce1fcac735165e5
adam01051 <lloki0105@gmail.com>
2026-05-01 16:47:41 +0900

    feat: modify batch module

---
+++ apps/nestar-batch/src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: async () => ({
+				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+			}),
+		}),
+	],
+
+	exports: [],
+})
+export class DatabaseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (connection.readyState === 1) {
+			console.log(`MongoDB connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`);
+		} else {
+			console.error('MongoDB connection failed');
+		}
+	}
+}
